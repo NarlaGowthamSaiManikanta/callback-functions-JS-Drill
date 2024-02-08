6307e37b13b8b7d315bf523fca9d0ff897cef718
Narla Gowtham Sai Manikanta <narla.manikanta.27.1@mountblue.tech>
2024-02-08 12:02:56 +0530

    Refactored code to promises - problem2.cjs, testProblem2.js

--- problem2.cjs
+++ problem2.cjs
@@ -1,116 +1,98 @@
 const fs = require('fs');
 
-function readingFileData(directory, filename, encoding) {
-    if (typeof filename === 'string') {
-        if (!directory) {
-            directory = '.';
+function readingFile(directory, filename, encoding) {
+    return new Promise((resolve, reject) => {
+        if (typeof filename === 'string') {
+            if (!directory) {
+                directory = '.';
+            }
+    
+            let filePath = `${directory}/${filename}`;
+    
+            if (!encoding) {
+                encoding = 'utf-8';
+            }
+    
+            fs.readFile(filePath, encoding, (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    console.log(`${filePath} is read successful`);
+                    resolve(data);
+                }
+            });
+        } else {
+            reject('Second Argument should be a string');
         }
+    });
+}
 
+function writingFile(directory, filename, data) {
+    return new Promise((resolve, reject) => {
         let filePath = `${directory}/${filename}`;
+        let filenamesFilePath = `${directory}/filenames.txt`;
 
-        if (!encoding) {
-            encoding = 'utf-8';
-        }
-
-        fs.readFile(filePath, encoding, (err, data) => {
+        fs.writeFile(filePath, data, (err) => {
             if (err) {
-                throw err;
+                reject(err);
+            } else {
+                console.log(`${filePath} file created.`);
+
+                fs.appendFile(filenamesFilePath, `${filename}\n`, (err) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        console.log(`${filenamesFilePath} updated.`);
+                        resolve(filename);
+                    }
+                });
             }
-
-            console.log(`${filePath} is read successful`);
-
-            data = data.toUpperCase();
-            writingFile(directory, `upper_${filename}`, data, readingUpperCaseFileData);
-        });
-    } else {
-        console.log('Second Argument should be a string');
-    }
-}
-
-function writingFile(directory, filename, data, cb) {
-    let filePath = `${directory}/${filename}`;
-    let filenamesFilePath = `${directory}/filenames.txt`;
-
-    fs.writeFile(filePath, data, (err) => {
-        if (err) {
-            throw err;
-        }
-
-        console.log(`${filePath} file created.`);
-
-        fs.appendFile(filenamesFilePath, `${filename}\n`, (err) => {
-            if (err) {
-                throw err;
-            }
-
-            console.log(`${filenamesFilePath} updated.`);
-            cb(directory, filename, 'utf-8');
         });
     });
 }
 
-function readingUpperCaseFileData(directory, filename, encoding) {
-    let filePath = `${directory}/${filename}`;
-
-    fs.readFile(filePath, encoding, (err, data) => {
-        if (err) {
-            throw err;
-        }
-
-        console.log(`${filePath} is read successful`);
-
-        data = data.toLowerCase();
-        data = data.replace(/([.?!])\s*(?=[a-z])/g, '$1\n');
-        writingFile(directory, `lower_${filename}`, data, readingLowerCaseFileData);
-    });
+function splitingIntoSentences(rawData) {
+    return rawData.replace(/([.?!])\s*(?=[a-z])/g, '$1\n');
 }
 
-function readingLowerCaseFileData(directory, filename, encoding) {
-    let filePath = `${directory}/${filename}`;
-
-    fs.readFile(filePath, encoding, (err, data) => {
-        if (err) {
-            throw err;
-        }
-
-        console.log(`${filePath} is read successful`);
-
-        sentences = data.split('\n');
-        sentences = sentences.sort();
-        sortingSentences(sentences, directory, filename);
-    });
-}
-
-function sortingSentences(sentences, directory, filename) {
-    data = sentences.sort()
+function sortingSentences(rawData) {
+    sentences = rawData.split('\n');
+    return sentences.sort()
         .reduce((accumulator, sentence) => {
             return accumulator + '\n' + sentence;
         });
-    writingFile(directory, `sorted_${filename}`, data, deleteCreatedFiles);
 }
 
-function deleteCreatedFiles(directory, filename, encoding) {
-    let filenamesFilePath = `${directory}/filenames.txt`;
+function deleteCreatedFiles(directory, encoding) {
+    return new Promise((resolve, reject) => {
+        let filenamesFilePath = `${directory}/filenames.txt`;
 
-    fs.readFile(filenamesFilePath, encoding, (err, data) => {
-        if (err) {
-            throw err;
-        }
+        fs.readFile(filenamesFilePath, encoding, (err, data) => {
+            if (err) {
+                reject(err);
+            }
 
-        data = data.split('\n').slice(0, -1);
-        data.push(filenamesFilePath.slice(5));
+            data = data.split('\n');
+            if (data[data.length - 1] === '') {
+                data = data.slice(0, -1);
+            }
+            data.push(filenamesFilePath.slice(directory.length+1));
 
-        data.forEach((fileName) => {
-            let filePath = 'data/' + fileName;
-            fs.unlink(filePath, (err) => {
-                if (err) {
-                    throw err;
-                }
-
-                console.log(`${filePath} file deleted`);
+            data.forEach((fileName) => {
+                let filePath = `${directory}/${fileName}`;
+                fs.unlink(filePath, (err) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        console.log(`${filePath} file deleted`);
+                        resolve(`${filePath} file deleted`);
+                    }
+                });
             });
         });
-    });
+    })
 }
 
-module.exports = readingFileData;
+module.exports = {
+    readingFile, writingFile, splitingIntoSentences, sortingSentences ,deleteCreatedFiles
+};
--- test/testProblem2.js
+++ test/testProblem2.js
@@ -1,7 +1,33 @@
-const readingFileData = require('../problem2.cjs');
+const {
+    readingFile, writingFile, splitingIntoSentences, sortingSentences, deleteCreatedFiles
+}
+    = require('../problem2.cjs');
 
 const directory = 'data';
 const filename = 'lipsum_1.txt';
 const encoding = 'utf-8';
 
-readingFileData(directory, filename, encoding);
+readingFile(directory, filename, encoding)
+.then((resolvedData) => {
+    return writingFile(directory, `upper_${filename}`, resolvedData.toUpperCase());
+})
+.then((createdFilename) => {
+    return readingFile(directory, createdFilename, encoding);
+})
+.then((resolvedData) => {
+    splittedData = splitingIntoSentences(resolvedData.toLowerCase());
+    return writingFile(directory, `lower_${filename}`, splittedData);
+})
+.then((createdFilename) => {
+    return readingFile(directory, createdFilename, encoding);
+})
+.then((resolvedData) => {
+    sortedData = sortingSentences(resolvedData);
+    return writingFile(directory, `sorted_${filename}`, sortedData);
+})
+.then((createdFilename) => {
+    return deleteCreatedFiles(directory, encoding);
+})
+.catch((rejectedReason) => {
+    console.log(rejectedReason);
+})
